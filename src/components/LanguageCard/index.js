9c62afeb617dc2efc6cc0c01fbbe406e3a2ff60d
davidryan-mu <davidryanyt@gmail.com>
2019-12-04 13:57:28 +0000

    Language Card now displays information based on database

--- src/components/LanguageCard/index.js
+++ src/components/LanguageCard/index.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../Firebase';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
-import flag_icon from '../../images/flag-icon.png';
+import german_icon from '../../images/german.png'
+import hindi_icon from '../../images/hindi.png'
+import irish_icon from '../../images/irish.png'
+import english_icon from '../../images/english.png'
+import arabic_icon from '../../images/arabic.png'
+import spanish_icon from '../../images/spanish.png'
+import welsh_icon from '../../images/welsh.png'
 
-export default class LanguageCard extends Component {
+class LanguageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: ""
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+      const currentUID = this.props.authUser.uid;
+      let currentUser = usersList.find(user => user.uid === currentUID);
+      this.setState({
+        language: currentUser.learnlang
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
   render() {
+    let language = this.state.language.toLowerCase();
+        let images = {
+          irish: irish_icon,
+          german: german_icon,
+          english: english_icon,
+          hindi: hindi_icon,
+          arabic: arabic_icon,
+          welsh: welsh_icon,
+          spanish: spanish_icon
+        }
     return (
       <div class="d-inline-block float-right"> 
         <Card style={{width: '25rem'}}>
           <Card.Header as="h5">Languages</Card.Header>
           <Card.Body>
-            <img class="d-inline-block float-left" src={flag_icon} width="50px"/>
+            <img class="d-inline-block float-left" src={images[language]} width="50px"/>
             <div class="d-inline-block mr-5 ml-3">
-              <Card.Title>Irish</Card.Title>
+              <Card.Title>{this.state.language}</Card.Title>
               <Card.Text>
                 Total XP: 59
               </Card.Text>
@@ -25,3 +67,5 @@ export default class LanguageCard extends Component {
     );
   }
 }
+
+export default withFirebase(LanguageCard);
