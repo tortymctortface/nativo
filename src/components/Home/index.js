33834617d6426c5e284ed31b25e5968767f165f5
davidryan-mu <davidryanyt@gmail.com>
2019-12-04 13:22:38 +0000

    WordOfTheDay Card is now dynamic
    The component now pulls the users learning language from the database and displays relevant information and images based on that

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import { withAuthorization } from '../Session';
+import { AuthUserContext, withAuthorization } from '../Session';
 
 import TopBarPlus from '../TopBarPlus/top-bar-plus';
 import CountdownCard from '../CountdownCard';
@@ -12,36 +12,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 const HomePage = () => (
-  <div>
-    <TopBarPlus />
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <TopBarPlus />
 
-    <div>
-      <div class="ml-5 mt-3 d-inline-block">
-        <h1>Welcome back!</h1>
-        <p>Jump straight back into learning. Practice makes perfect.</p>
+        <div>
+          <div class="ml-5 mt-3 d-inline-block">
+            <h1>Welcome back!</h1>
+            <p>Jump straight back into learning. Practice makes perfect.</p>
+          </div>
+
+          <div class="mr-5 mt-4 d-inline-block float-right">
+            <Link to={{pathname: '/chat'}}>
+              <Button variant="warning" as="input" type="submit" value="Chat" style={{ width: '35rem', margin: 'auto'}}></Button>
+            </Link>
+          </div>
+
+          <br /><br />
+        </div>
+
+        <div>
+          <WordOfTheDayCard authUser={authUser}/>
+
+          <CountdownCard />
+
+          <br /><br />
+        </div>
+
+        <div>
+          <XPProgressCard />
+        </div>
       </div>
-
-      <div class="mr-5 mt-4 d-inline-block float-right">
-        <Link to={{pathname: '/chat'}}>
-          <Button variant="warning" as="input" type="submit" value="Chat" style={{ width: '35rem', margin: 'auto'}}></Button>
-        </Link>
-      </div>
-
-      <br /><br />
-    </div>
-
-    <div>
-      <WordOfTheDayCard />
-
-      <CountdownCard />
-
-      <br /><br />
-    </div>
-
-    <div>
-      <XPProgressCard />
-    </div>
-  </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser => !!authUser;
--- src/components/WordOfTheDayCard/index.js
+++ src/components/WordOfTheDayCard/index.js
@@ -1,28 +1,114 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../Firebase';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 
-import flag_icon from '../../images/flag-icon.png';
+import german_icon from '../../images/german.png'
+import hindi_icon from '../../images/hindi.png'
+import irish_icon from '../../images/irish.png'
+import english_icon from '../../images/english.png'
+import arabic_icon from '../../images/arabic.png'
+import spanish_icon from '../../images/spanish.png'
+import welsh_icon from '../../images/welsh.png'
 
-export default class WordOfTheDayCard extends Component {
+class WordOfTheDayCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: ""
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.props.firebase.users().on('value', snapshot => {
+      const usersObject = snapshot.val();
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+      const currentUID = this.props.authUser.uid;
+      let currentUser = usersList.find(user => user.uid === currentUID);
+      this.setState({
+        language: currentUser.learnlang
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
     render() {
+      let language = this.state.language.toLowerCase();
+      const words = {
+        irish: {
+          word: "Fáilte",
+          desc: "This word also translates to ‘joy, bliss or happiness’, but is mostly used to say ‘welcome’.",
+          pronounce: "Pronounce it like this: fawl-cha"
+        },
+
+        english: {
+          word: "Umbrella",
+          desc: "A device consisting of a circular canopy of cloth on a folding metal frame supported by a central rod, used as protection against rain.",
+          pronounce: "Pronounce it like this: uhm-breh-la"
+        },
+
+        welsh: {
+          word: "Balŵn",
+          desc: "A small coloured rubber bag which is inflated with air and then sealed at the neck, used as a child's toy or a decoration.",
+          pronounce: "Pronounce it like this: bahl-un"
+        },
+
+        spanish: {
+          word: "Nevado",
+          desc: "This word means 'snowy'.",
+          pronounce: "Pronounce it like this: nev-a-doh"
+        },
+
+        hindi: {
+          word: "रोशनदान",
+          desc: "This word means 'skylight'",
+          pronounce: "Pronounce it like this: ro-shan-a-daan"
+        },
+
+        arabic: {
+          word: "إن شاء الله‎,",
+          desc: "the Arabic language expression for 'God willing' or 'if God wills'. The phrase is commonly used by Muslims, Arab Christians, and Arabic-speakers of other religions to refer to events that one hopes will happen in the future.",
+          pronounce: "Pronounce it like this: in-sha-all-ha‎"
+        },
+
+        german: {
+          word: "Unkaputtbar",
+          desc: "It basically means 'unbreakable' or 'indestuctible' and is a very new addition to the German language.",
+          pronounce: "Pronounce it like this: un‧ka‧putt‧bar"
+        }
+      };
+
+      let images = {
+        irish: irish_icon,
+        german: german_icon,
+        english: english_icon,
+        hindi: hindi_icon,
+        arabic: arabic_icon,
+        welsh: welsh_icon,
+        spanish: spanish_icon
+      }
       return (
         <div class="d-inline-block ml-5"> 
           <Card style={{width: '35rem'}}>
             <Card.Header as="h5">
-                <img class="d-inline-block float-left mt-1 mr-2" src={flag_icon} width="20px"/>
-                Word Of the Day - Irish
+                {language !== "" && <img class="d-inline-block float-left mt-1 mr-2" src={images[language]} width="25px"/>}
+                Word Of the Day - {this.state.language}
             </Card.Header>
             <Card.Body>
               <div class="d-inline-block">
-                <Card.Title>Fáilte</Card.Title>
+                <Card.Title>{language !== "" && words[language].word}</Card.Title>
                 <Card.Text>
-                This word also translates to ‘joy, bliss or happiness’, but is mostly used to say ‘welcome’.
+                {language !== "" && words[language].desc}
 
                 <br /> <br />
 
-                Pronounce it like this: fawl-cha
+                {language !== "" && words[language].pronounce}
                 </Card.Text>
               </div>
             </Card.Body>
@@ -31,3 +117,5 @@ export default class WordOfTheDayCard extends Component {
       );
     }
   }
+
+  export default withFirebase(WordOfTheDayCard);
